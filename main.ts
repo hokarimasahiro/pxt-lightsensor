875c965f1f38efde0006f7997e93c5e950731ebb
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-05-17 05:37:49 +0900

    test

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 /**
  * Propotional Font Display blocks
  */
-//% weight=100 color=#0fbc11 icon="\u270f" block="Proportional Font"
+//% weight=100 color=#0fbc11 icon="\u270f" block="light sensor"
 enum onoff {
     //% block=off
     off = 0,
@@ -11,6 +11,13 @@ enum onoff {
 namespace lightsensor {
     let init = 0
     let lastLightLevel:number
+    /**
+     * TODO:明るさの変化を検出
+     * @param level 数値。, eg: 0
+     * @param handler 関数。, eg: action
+     */
+    //% blockId="明るさの変化を検出" block="明るさの変化を検出 %level %handler"
+    //% weight=100 blockGap=8
     export function onBrightnessChange(level: onoff, handler: Action) {
         control.onEvent(3300, level, handler);
         if (init == 0) {
