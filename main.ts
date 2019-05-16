38f6954068006d682a7217b9839561c1902edaa8
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-05-17 05:27:25 +0900

    new

--- main.ts
+++ main.ts
@@ -1,7 +1,30 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * Propotional Font Display blocks
+ */
+//% weight=100 color=#0fbc11 icon="\u270f" block="Proportional Font"
+enum onoff {
+    //% block=off
+    off = 0,
+    //% block=on
+    on = 1
+}
+namespace lightsensor {
+    let init = 0
+    let lastLightLevel:number
+    export function onBrightnessChange(level: onoff, handler: Action) {
+        control.onEvent(0x3300, level, handler);
+        if (init == 0) {
+            lastLightLevel = input.lightLevel()
+            control.inBackground(() => {
+                while (true) {
+                    const lightLevel = input.lightLevel()
+                    if (lightLevel != lastLightLevel) {
+                        lastLightLevel = lightLevel
+                        control.raiseEvent(0x3300, lastLightLevel)
+                    }
+                    basic.pause(50);
+                }
+            })
+        }
+    }
+}
